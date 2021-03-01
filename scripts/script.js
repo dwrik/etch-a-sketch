f3cdf5ab9e561c5c014e300a3570aaa8e9a976e3
Wrik Das <wrikdas22@gmail.com>
2021-03-01 15:51:11 +0530

    Implemented coloring of grids on hover

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,37 @@ let gridSize = 400; // in pixels
 const sizeButton = document.querySelector('#size');
 sizeButton.addEventListener('click', changeSize);
 
+const clearButton = document.querySelector('#size');
+clearButton.addEventListener('click', clearGrid);
+
+generateGrid(size);
+
+function clearGrid(event) {
+}
+
 function changeSize(event) {
     size = getSize();
     generateGrid(size);
+}
+
+function generateGrid(size) {
+    const divSize = Math.round(gridSize / size);
+    const grid = document.querySelector('#grid');
+
+    destroyExistingGrid(grid);
+
+    grid.style['grid-template-rows'] = `repeat(${size}, ${divSize}px)`;
+    grid.style['grid-template-columns'] = `repeat(${size}, ${divSize}px)`;
+
+    for (let i=0; i<size**2; i++) {
+        const div = getGridSquare();
+        grid.appendChild(div);
+    }
+}
+
+function destroyExistingGrid(grid) {
+    const gridSquares = [ ...document.querySelectorAll('#grid div') ];
+    gridSquares.forEach(square => grid.removeChild(square));
 }
 
 function getSize() {
@@ -18,31 +46,11 @@ function getSize() {
     return userInput;
 }
 
-function destroyExistingGrid(grid) {
-    const gridSquares = [ ...document.querySelectorAll('#grid div') ];
-    gridSquares.forEach(square => grid.removeChild(square));
-}
-
-function generateGrid(size) {
-    const divSize = Math.round(gridSize / size);
-    const grid = document.querySelector('#grid');
-
-    destroyExistingGrid(grid);
-
-    grid.style['display'] = 'grid';
-    grid.style['grid-template-rows'] = `repeat(${size}, ${divSize}px)`;
-    grid.style['grid-template-columns'] = `repeat(${size}, ${divSize}px)`;
-
-    for (let i=0; i<size**2; i++) {
-        const div = getGridSquare();
-        grid.appendChild(div);
-    }
-}
-
 function getGridSquare() {
     const divSize = Math.round(gridSize / size);
     const square = document.createElement('div');
 
+    square.addEventListener('mouseover', changeColor);
     square.style.width = `${divSize}px`;
     square.style.height = square.style.width;
     square.style.border = '1px solid black';
@@ -51,4 +59,7 @@ function getGridSquare() {
     return square;
 }
 
-generateGrid(size);
+function changeColor(event) {
+    const div = event.target;
+    div.style.backgroundColor = `black`;
+}
